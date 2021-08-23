68c307806f78f8e92bc7fd105668270758b9a537
Karan Kumar <karankumar072508@gmail.com>
2021-08-23 11:51:32 +0530

    first commit

---
+++ EPM.Core/CustomerManagement/CustomerUsers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EPM.Core.CustomerManagement
+{
+    public class CustomerUsers
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Customer Name is required..")]
+        [MaxLength(100, ErrorMessage = "UserName length is too Long..")]
+        [MinLength(6, ErrorMessage = "UserName length is too short..")]
+        public string CustName { get; set; }
+
+        [Required(ErrorMessage = "Customer Phone is required..")]
+        public String Phone { get; set; }
+        public string EmpCode { get; set; }
+
+        [Required(ErrorMessage = "Customer Mail is required..")]
+        public string Email { get; set; }
+    }
+}
---
+++ EPM.Core/EmployeeManagement/EmployeeUsers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EPM.Core.EmployeeManagement
+{
+    public class EmployeeUsers
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Employee Name is required..")]
+        [MaxLength(100, ErrorMessage = "UserName length is too Long..")]
+        [MinLength(6, ErrorMessage = "UserName length is too short..")]
+        public string EmpName { get; set; }
+
+        [Required(ErrorMessage = "Employee Phone is required..")]
+        public String Phone { get; set; }
+        public string EmpCode { get; set; }
+
+        [Required(ErrorMessage = "Employee Mail is required..")]
+        public string Email { get; set; }
+    }
+}
